859bce7f4bc3db36d880c818a765888636229417
Nicholas Nadel <nicholasnadel@gmail.com>
2019-06-28 08:45:12 -0700

    add Slate Build.xslt to version control

--- dist/assets.8f4429fc.js
+++ dist/assets.8f4429fc.js
@@ -228,7 +228,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52151" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65070" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
