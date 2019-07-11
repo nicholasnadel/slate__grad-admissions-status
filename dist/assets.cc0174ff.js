ee66bb6a348f560287e6ef77fde661b5f7554455
Nicholas Nadel <nicholasnadel@gmail.com>
2019-07-11 11:14:23 -0700

    adjust application-questions widget margin

---
+++ dist/assets.cc0174ff.js
@@ -0,0 +1,8 @@
+parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"UAnV":[function(require,module,exports) {
+
+},{}],"Ufbi":[function(require,module,exports) {
+var e=$("#application-checklist").html();e=(e=(e=e.replace(/Received/g,"<span class='received'>Received</span>")).replace(/Required/g,"<span class='required'>Required</span>")).replace(/Optional/g,"<span class='optional'>Optional</span>"),$("#application-checklist").html(e);
+},{}],"zBiA":[function(require,module,exports) {
+"use strict";require("./master.scss"),require("./table.js");
+},{"./master.scss":"UAnV","./table.js":"Ufbi"}]},{},["zBiA"], null)
+//# sourceMappingURL=assets.cc0174ff.js.map
