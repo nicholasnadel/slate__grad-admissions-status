aa16d869b3fb3c3551a8e32b5af057b33e81fbf2
Nicholas Nadel <nicholasnadel@gmail.com>
2019-07-11 11:27:38 -0700

    troubleshooting netlify cache

--- dist/assets.8f4429fc.js
+++ dist/assets.8f4429fc.js
@@ -201,7 +201,7 @@ $("#application-checklist").html(t);
 require("./master.scss");
 
 require("./table.js");
-},{"./master.scss":"assets/master.scss","./table.js":"assets/table.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./master.scss":"assets/master.scss","./table.js":"assets/table.js"}],"../../.config/yarn/global/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -229,7 +229,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58221" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53545" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -404,5 +404,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["node_modules/parcel-bundler/src/builtins/hmr-runtime.js","assets/index.js"], null)
+},{}]},{},["../../.config/yarn/global/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","assets/index.js"], null)
 //# sourceMappingURL=/assets.8f4429fc.js.map
