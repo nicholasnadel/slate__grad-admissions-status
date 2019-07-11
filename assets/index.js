f27f065699522e526af3e383abc52757d76534fa
Nicholas Nadel <nicholasnadel@gmail.com>
2019-07-11 10:43:44 -0700

    add parcel-bundler to push to netlify

--- assets/index.js
+++ assets/index.js
@@ -1,2 +1,3 @@
 // https://github.com/parcel-bundler/parcel/issues/1402#issuecomment-390678078
 import './master.scss';
+import './table.js';
---
+++ assets/login.js
@@ -0,0 +1,5 @@
+$(function () {
+  $("#global ul li").addClass("cr slate-login fa fa-address-card-0");
+  $("#global ul li").prependTo("#c_omni-nav-v2.cr .c_login-trigger.cr .c_login-menu.cr ul");
+
+});
